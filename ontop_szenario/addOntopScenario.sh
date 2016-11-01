314eba3c9a72e6aa7a40832b64d4612ba1a663c6
Iranox <aasgar@arcor.de>
2016-11-01 20:25:48 +0100

    Remove files

--- ontop_szenario/addOntopScenario.sh
+++
@@ -1,2 +0,0 @@
-git submodule add  -b feature/teiid --force https://github.com/Iranox/mapbench-data.git data
-cat  docker-compose.yml scripts/add.txt >> docker-compose.yml
--- ontop_szenario/addOntopTeiidMySQL.sh
+++
@@ -1,4 +0,0 @@
-git submodule add  -b feature/teiid --force https://github.com/Iranox/mapbench-data.git data
-cat  docker-compose.yml scripts/addTeiidMysql.txt >> docker-compose.yml
-cat  docker-compose.yml scripts/add.txt >> docker-compose.yml
-
