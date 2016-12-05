98d2735ca830f4dd37c96b2377543d42f866de1b
Iranox <aasgar@arcor.de>
2016-12-05 23:51:59 +0100

    Update

--- ontop_teiid/startTeiidOntop.sh
+++ ontop_teiid/startTeiidOntop.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.yml  build
-docker-compose -f ../scenarios/data/docker-compose.yml  build
+#docker-compose -f docker-compose.yml  build
+#docker-compose -f ../scenarios/data/docker-compose.yml  build
 docker-compose -f ../scenarios/data/docker-compose.yml -f docker-compose.yml up
