c20a84f400ba1d56d5a94fe58a3f58fe58ba6392
Iranox <aasgar@arcor.de>
2016-12-12 22:19:38 +0100

    Update

---
+++ scenarios/semagrow/startSemagrow.sh
@@ -0,0 +1,5 @@
+docker-compose -f ../data/scenario/base/docker-compose.yml -f docker-compose.yml up -d
+docker-compose -f  ../data/scenario/base/docker-compose.yml  -f docker-compose.yml \
+     run base_bsbm_generator sh /bsbm/scripts/bsbm_generator/generateData.sh $1
+docker-compose -f  ../data/scenario/base/docker-compose.yml  -f docker-compose.yml \
+    run base_mysql sh /bsbm/scripts/mysql/import_sql.sh
